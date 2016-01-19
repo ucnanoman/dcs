c1e8a090c3f16b8d701b3dac9bb49bf48e032cd1
Peinthor Rene <peinthor@gmail.com>
2016-01-19 15:00:00 +0100

    wip mission loading

--- dcs/mission.py
+++ dcs/mission.py
@@ -1,4 +1,55 @@
+import zipfile
+
+
+class Options():
+    difficulty = {}
+    playerName = ""
+    graphics = {}
+    plugins = {}
+    views = {}
+    sound = {}
+    miscellaneous = {}
+
+
+class Weather():
+    pass
+
+
+class Coalition():
+    blue = {}
+    red = {}
+    neutral = {}
 
 
 class Mission():
-    pass
+    trig = []
+    triggers = {}
+    result = []
+    maxDictId = 0
+    groundControl = {}
+    usedModules = {}
+    weather = Weather()
+    theatre = "Caucasus"
+    needModules = {}
+    coalitions = {}
+    coalition = Coalition()  # unit descriptions
+    version = 9
+    goals = {}
+    currentKey = 0
+    start_time = 43200
+
+    descriptionText = ""
+    descriptionBlueTask = ""
+    descriptionRedTask = ""
+    pictureFileNameR = ""
+    pictureFileNameB = ""
+
+    options = Options()
+    forcedOptions = {}
+    failures = {}
+
+    def loadFile(filename):
+        with zipfile.ZipFile(filename, 'r') as miz:
+            print(miz.namelist)
+
+        return True
