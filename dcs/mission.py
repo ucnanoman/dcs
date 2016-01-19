41d45e34a69c2a4539a999ae6c2b5db00ff5fa3d
Peinthor Rene <peinthor@gmail.com>
2016-01-19 21:47:50 +0100

    a bit of file loading

--- dcs/mission.py
+++ dcs/mission.py
@@ -1,4 +1,5 @@
 import zipfile
+import lua
 
 
 class Options():
@@ -48,8 +49,33 @@ class Mission():
     forcedOptions = {}
     failures = {}
 
-    def loadFile(filename):
+    def __init__(self, filename):
+        self.loadFile(filename)
+
+    def loadFile(self, filename):
+        mission_dict = {}
+        options_dict = {}
+        warehouse_dict = {}
         with zipfile.ZipFile(filename, 'r') as miz:
-            print(miz.namelist)
+            with miz.open('mission') as mfile:
+                mission_data = mfile.read()
+
+                mission_data = mission_data.decode()
+                mission_dict = lua.loads(mission_data)
+                print(mission_dict)
+
+            with miz.open('options') as optfile:
+                opt_data = optfile.read()
+
+                opt_data = opt_data.decode()
+                options_dict = lua.loads(opt_data)
+                print(options_dict)
+
+            with miz.open('warehouses') as warefile:
+                ware_data = warefile.read()
+
+                ware_data = ware_data.decode()
+                warehouse_dict = lua.loads(ware_data)
+                print(warehouse_dict)
 
         return True
