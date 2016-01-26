123b7efa4bcf00c3a56bc0236e1fdbfb112d3c59
Peinthor Rene <peinthor@gmail.com>
2016-01-26 23:02:08 +0100

    sort keys before serializing them

--- lua/serialize.py
+++ lua/serialize.py
@@ -1,22 +1,23 @@
 def dumps(value, varname=None, indent=None):
-    NL = "\n" if indent else ""
-    s = varname + '=' + NL if varname else ''
+    nl = "\n" if indent else ""
+    s = varname + '=' + nl if varname else ''
     indentcount = 0 if indent is None else indent
 
     if isinstance(value, dict):
         e = []
-        for key in value:
+        for key in sorted(value.keys()):
             child = value[key]
             KNL = "\n" if indent and isinstance(child, (dict, list)) else ""
             selem = '\t' * indentcount
-            selem += '["{key}"]={nl}'.format(key=key, nl=KNL)
+            skey = key if isinstance(key, int) else '"{key}"'.format(key=key)
+            selem += '[{key}]={nl}'.format(key=skey, nl=KNL)
             selem += dumps(value[key], indent=indent + 1 if indent else None)
             e.append(selem)
-        s += '\t' * (indent-1) if NL else ""
+        s += '\t' * (indent-1) if nl else ""
         s += "{"
         if e:
-            s += NL + ",{nl}".format(nl=NL).join(e)
-        s += NL + '\t' * (indentcount-1) + "}"
+            s += nl + ",{nl}".format(nl=nl).join(e)
+        s += nl + '\t' * (indentcount-1) + "}"
     elif isinstance(value, list):
         e = []
         i = 1
@@ -25,11 +26,11 @@ def dumps(value, varname=None, indent=None):
             selem += dumps(v, indent=indent + 1 if indent else None)
             e.append(selem)
             i += 1
-        s += '\t' * (indent-1) if NL else ""
+        s += '\t' * (indent-1) if nl else ""
         s += "{"
         if e:
-            s += NL + ",{nl}".format(nl=NL).join(e)
-        s += NL + '\t' * (indentcount-1) + "}"
+            s += nl + ",{nl}".format(nl=nl).join(e)
+        s += nl + '\t' * (indentcount-1) + "}"
     elif isinstance(value, str):
         s += '"{val}"'.format(val=value.replace('"', '\\"'))
     elif isinstance(value, bool):
