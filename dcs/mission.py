1b58bc530303e0fb770a39809eb4fdd350965abb
Peinthor Rene <peinthor@gmail.com>
2016-01-27 15:22:36 +0100

    load of import additions

--- dcs/mission.py
+++ dcs/mission.py
@@ -2,153 +2,6 @@ import zipfile
 import lua
 
 
-class Options:
-    def __init__(self, opts={}):
-        self.options = opts
-
-    def __repr__(self):
-        return repr(self.options)
-
-
-class Weather:
-
-    def dict(self):
-        return {}
-
-    def __str__(self):
-        return lua.dumps({}, None, 1)
-
-
-class Task:
-    CAS = "CAS"
-    CAP = "CAP"
-
-
-class VehicleType:
-    M818 = "M 818"
-
-
-class PlaneType:
-    A10C = "A-10C"
-
-
-class Skill:
-    AVERAGE = "Average"
-    HIGH = "High"
-
-
-class Unit:
-    def __init__(self):
-        self.type = ""
-        self.x = 0
-        self.y = 0
-        self.heading = 0
-        self.id = 0
-        self.skill = Skill.AVERAGE
-        self.player_can_drive = False
-        self.transportable = {"randomTransportable": False}
-        self.name = String()
-
-
-class Plane(Unit):
-    def __init__(self):
-        self.livery_id = ""
-        self.parking = None
-        self.psi = ""
-        self.callsign = {}
-        self.onboard_num = "010"
-
-
-class StaticType:
-    AMMUNITION_DEPOT = ".Ammunition depot"
-
-
-class Static(Unit):
-    def __init__(self):
-        self.category = "Warehouses"
-        self.can_cargo = False
-
-
-class Point:
-    def __init__(self):
-        self.alt = 0
-        self.alt_type = "BARO"
-        self.type = ""
-        self.name = String()
-        self.ETA = 0
-        self.ETA_locked = True
-        self.formation_template = ""
-        self.speed_locked = True
-        self.x = 0
-        self.y = 0
-        self.speed = 0
-        self.action = "Offroad"
-        self.task = []
-        self.properties = None
-
-
-class Group:
-    def __init__(self):
-        self.task = ""
-        self.tasks = []
-        self.uncontrolled = False
-        self.id = 0
-        self.hidden = False
-        self.units = []
-        self.x = 0
-        self.y = 0
-        self.start_time = 0
-        self.spans = []
-        self.points = []
-        self.name = String()
-        self.frequency = None
-
-
-class Country:
-    def __init__(self, _id, name, vehicle_group=[], plane_group=[], static_group=[]):
-        self.id = _id
-        self.name = name
-
-    def name(self):
-        return self.name
-
-    def dict(self):
-        d = {}
-        d["name"] = self.name
-        d["id"] = self.id
-        return d
-
-
-class Coalition:
-    def __init__(self, name, bullseye=None):
-        self.name = name
-        self.countries = []
-        self.bullseye = bullseye
-        self.nav_points = []
-
-    def set_bullseye(self, bulls):
-        self.bullseye = bulls
-
-    def add_country(self, country):
-        self.countries.append(country)
-
-    def remove_country(self, name):
-        return self.countries.pop(name)
-
-    def dict(self):
-        d = {}
-        d["name"] = self.name
-        if self.bullseye:
-            d["bullseye"] = self.bullseye
-        d["country"] = {}
-        i = 1
-        for country in self.countries:
-            d["country"][i] = country.dict()
-            i += 1
-        d["nav_points"] = {}
-        return d
-
-
 class String:
     def __init__(self, _id='', translation=None, lang='DEFAULT'):
         self.translation = translation
@@ -194,6 +47,336 @@ class Translation:
 
     def __repr__(self):
         return repr(self.strings)
+
+
+class Options:
+    def __init__(self, opts={}):
+        self.options = opts
+
+    def __repr__(self):
+        return repr(self.options)
+
+
+class Weather:
+
+    def dict(self):
+        return {}
+
+    def __str__(self):
+        return lua.dumps({}, None, 1)
+
+
+class Task:
+    CAS = "CAS"
+    CAP = "CAP"
+
+
+class VehicleType:
+    M818 = "M 818"
+
+
+class PlaneType:
+    A10C = "A-10C"
+
+
+class Skill:
+    AVERAGE = "Average"
+    HIGH = "High"
+
+
+class MapPosition:
+    def __init__(self, x, y):
+        self._x = x
+        self._y = y
+
+    def x(self):
+        return self._x
+
+    def y(self):
+        return self._y
+
+
+class Unit:
+    def __init__(self, id=None, name=None, type=""):
+        self.type = ""
+        self.x = 0
+        self.y = 0
+        self.heading = 0
+        self.id = id
+        self.skill = Skill.AVERAGE
+        self.player_can_drive = False
+        self.transportable = {"randomTransportable": False}
+        self.name = name if name else String()
+
+    def set_position(self, pos):
+        self.x = pos.x()
+        self.y = pos.y()
+
+    def dict(self):
+        d = {}
+        d["type"] = self.type
+        d["x"] = self.x
+        d["y"] = self.y
+        d["heading"] = self.heading
+        d["skill"] = self.skill
+        d["unitId"] = self.id
+        d["name"] = self.name.id()
+        d["transportable"] = self.transportable
+        d["playerCanDrive"] = self.player_can_drive
+        return d
+
+
+class Plane(Unit):
+    def __init__(self, id=None, name=None, type=""):
+        super(Plane, self).__init__(id, name, type)
+        self.livery_id = ""
+        self.parking = None
+        self.psi = ""
+        self.callsign = {}
+        self.onboard_num = "010"
+        self.alt = 0
+        self.alt_type = "BARO"
+        self.payload = {}
+
+    def dict(self):
+        d = super(Plane, self).dict()
+        d["livery_id"] = self.livery_id
+        d["psi"] = self.psi
+        d["onboard_num"] = self.onboard_num
+        return d
+
+
+class StaticType:
+    AMMUNITION_DEPOT = ".Ammunition depot"
+
+
+class Static(Unit):
+    def __init__(self):
+        self.category = "Warehouses"
+        self.can_cargo = False
+
+
+class Point:
+    def __init__(self):
+        self.alt = 0
+        self.alt_type = "BARO"
+        self.type = ""
+        self.name = String()
+        self.ETA = 0
+        self.ETA_locked = True
+        self.formation_template = ""
+        self.speed_locked = True
+        self.x = 0
+        self.y = 0
+        self.speed = 0
+        self.action = "Offroad"
+        self.task = {}
+        self.properties = None
+
+    def dict(self):
+        d = {}
+        d["alt"] = self.alt
+        d["alt_type"] = self.alt_type
+        d["type"] = self.type
+        d["name"] = self.name.id()
+        d["ETA"] = self.ETA
+        d["ETA_locked"] = self.ETA_locked
+        d["speed"] = self.speed
+        d["speed_locked"] = self.speed_locked
+        d["formation_template"] = self.formation_template
+        d["x"] = self.x
+        d["y"] = self.y
+        d["action"] = self.action
+        d["task"] = self.task
+        return d
+
+
+class Group:
+    def __init__(self, name=None):
+        self.tasks = []
+        self.uncontrolled = False
+        self.id = 0
+        self.hidden = False
+        self.visible = False
+        self.units = []
+        self.x = 0
+        self.y = 0  # for now take pos from first unit
+        self.spans = []
+        self.points = []
+        self.name = name if name else String()
+        self.frequency = None
+
+    def add_unit(self, unit: Unit):
+        self.units.append(unit)
+
+    def add_point(self, point: Point):
+        self.points.append(point)
+
+    def add_span(self, pos):
+        self.spans.append({"x": pos.x, "y": pos.y})
+
+    def dict(self):
+        d = {}
+        d["visible"] = self.visible
+        d["hidden"] = self.hidden
+        d["name"] = self.name.id()
+        d["groupId"] = self.id
+        if self.units:
+            d["x"] = self.units[0].x
+            d["y"] = self.units[0].y
+            d["units"] = {}
+            i = 1
+            for unit in self.units:
+                d["units"][i] = unit.dict()
+        if self.points:
+            d["route"] = {"points": {}}
+            i = 1
+            for point in self.points:
+                d["route"]["points"][i] = point.dict()
+                i += 1
+        if self.spans:
+            d["route"]["spans"] = {}
+            i = 1
+            for spawn in self.spans:
+                d["route"]["spans"][i] = spawn
+                i += 1
+        return d
+
+
+class MovingGroup(Group):
+    def __init__(self, task="", name=None, start_time=0):
+        super(MovingGroup, self).__init__(name)
+        self.task = task
+        self.start_time = start_time
+
+    def dict(self):
+        d = super(MovingGroup, self).dict()
+        d["task"] = self.task
+        d["start_time"] = self.start_time
+        return d
+
+
+class VehicleGroup(MovingGroup):
+    def __init__(self, task="", name=None, start_time=0):
+        super(VehicleGroup, self).__init__(task, name, start_time)
+        self.modulation = 0
+        self.frequency = 251
+        self.communication = True
+
+    def dict(self):
+        d = super(VehicleGroup, self).dict()
+        d["modulation"] = self.modulation
+        d["frequency"] = self.frequency
+        d["communication"] = self.communication
+        return d
+
+
+class PlaneGroup(MovingGroup):
+    def __init__(self, task="", name=None, start_time=0):
+        super(PlaneGroup, self).__init__(task, name, start_time)
+        self.modulation = 0
+        self.frequency = 251
+        self.communication = True
+
+    def dict(self):
+        d = super(PlaneGroup, self).dict()
+        d["modulation"] = self.modulation
+        d["frequency"] = self.frequency
+        d["communication"] = self.communication
+        return d
+
+
+class StaticGroup(Group):
+    def __init__(self, task="", name=None, start_time=0):
+        super(StaticGroup, self).__init__(task, name, start_time)
+        self.dead = False
+        self.heading = 0
+
+    def dict(self):
+        d = super(StaticGroup, self).dict()
+        d["dead"] = self.dead
+        d["heading"] = self.heading
+        return d
+
+
+class Country:
+    def __init__(self, _id, name, vehicle_group=None, plane_group=None, static_group=None):
+        self.id = _id
+        self.name = name
+        self.vehicle_group = vehicle_group if vehicle_group else []
+        self.plane_group = plane_group if plane_group else []
+        self.static_group = static_group if static_group else []
+
+    def name(self):
+        return self.name
+
+    def add_vehicle_group(self, vgroup):
+        self.vehicle_group.append(vgroup)
+
+    def add_plane_group(self, pgroup):
+        self.plane_group.append(pgroup)
+
+    def add_static_group(self, sgroup):
+        self.static_group.append(sgroup)
+
+    def dict(self):
+        d = {}
+        d["name"] = self.name
+        d["id"] = self.id
+
+        if self.vehicle_group:
+            d["vehicle"] = {"group": {}}
+            i = 1
+            for vgroup in self.vehicle_group:
+                d["vehicle"]["group"][i] = vgroup.dict()
+                i += 1
+        if self.plane_group:
+            d["plane"] = {"group": {}}
+            i = 1
+            for plane_group in self.plane_group:
+                d["plane"]["group"][i] = plane_group.dict()
+                i += 1
+
+        if self.static_group:
+            d["static"] = {"group": {}}
+            i = 1
+            for static_group in self.static_group:
+                d["static"]["group"][i] = static_group.dict()
+                i += 1
+        return d
+
+    def __str__(self):
+        return str(self.id) + "," + self.name + "," + str(self.vehicle_group)
+
+
+class Coalition:
+    def __init__(self, name, bullseye=None):
+        self.name = name
+        self.countries = []
+        self.bullseye = bullseye
+        self.nav_points = []
+
+    def set_bullseye(self, bulls):
+        self.bullseye = bulls
+
+    def add_country(self, country):
+        self.countries.append(country)
+
+    def remove_country(self, name):
+        return self.countries.pop(name)
+
+    def dict(self):
+        d = {}
+        d["name"] = self.name
+        if self.bullseye:
+            d["bullseye"] = self.bullseye
+        d["country"] = {}
+        i = 1
+        for country in self.countries:
+            d["country"][i] = country.dict()
+            i += 1
+        d["nav_points"] = {}
+        return d
 
 
 class Mission:
@@ -261,6 +444,26 @@ class Mission:
             'F-86F Sabre AI by Eagle Dynamics': True
         }
 
+    def _import_point(self, group: Group, imp_group) -> Group:
+        for imp_point_idx in imp_group["route"]["points"]:
+            imp_point = imp_group["route"]["points"][imp_point_idx]
+            point = Point()
+            point.alt = imp_point["alt"]
+            point.alt_type = imp_point["alt_type"]
+            point.type = imp_point["type"]
+            point.x = imp_point["x"]
+            point.y = imp_point["y"]
+            point.action = imp_point["action"]
+            point.ETA_locked = imp_point["ETA_locked"]
+            point.ETA = imp_point["ETA"]
+            point.formation_template = imp_point["formation_template"]
+            point.speed_locked = imp_point["speed_locked"]
+            point.speed = imp_point["speed"]
+            point.name = self.translation.get_string(imp_point["name"])
+            point.task = imp_point["task"]
+            group.add_point(point)
+        return group
+
     def load_file(self, filename):
         mission_dict = {}
         options_dict = {}
@@ -314,10 +517,55 @@ class Mission:
             col = Coalition(key, imp_col["bullseye"])
             for country_idx in imp_col["country"]:
                 imp_country = imp_col["country"][country_idx]
+                country = Country(imp_country["id"], imp_country["name"])
+
                 if "vehicle" in imp_country:
-                    for vgroup in imp_country["vehicle"]["group"]:
-                        print(vgroup)
-                col.add_country(Country(imp_country["id"], imp_country["name"]))
+                    for vgroup_idx in imp_country["vehicle"]["group"]:
+                        vgroup = imp_country["vehicle"]["group"][vgroup_idx]
+                        vg = VehicleGroup(vgroup["task"], self.translation.get_string(vgroup["name"]), vgroup["start_time"])
+
+                        self._import_point(vg, vgroup)
+
+                        # units
+                        for imp_unit_idx in vgroup["units"]:
+                            imp_unit = vgroup["units"][imp_unit_idx]
+                            unit = Unit(id=imp_unit["unitId"], name=self.translation.get_string(imp_unit["name"]))
+                            unit.set_position(MapPosition(imp_unit["x"], imp_unit["y"]))
+                            unit.heading = imp_unit["heading"]
+                            unit.type = imp_unit["type"]
+                            unit.skill = imp_unit["skill"]
+                            unit.x = imp_unit["x"]
+                            unit.y = imp_unit["y"]
+                            vg.add_unit(unit)
+                        country.add_vehicle_group(vg)
+
+                if "plane" in imp_country:
+                    for pgroup_idx in imp_country["plane"]["group"]:
+                        pgroup = imp_country["plane"]["group"][pgroup_idx]
+                        plane_group = PlaneGroup(pgroup["task"], self.translation.get_string(pgroup["name"]), pgroup["start_time"])
+                        plane_group.frequency = pgroup["frequency"]
+                        plane_group.modulation = pgroup["modulation"]
+                        plane_group.communication = pgroup["communication"]
+                        plane_group.uncontrolled = pgroup["uncontrolled"]
+
+                        self._import_point(plane_group, pgroup)
+
+                        # units
+                        for imp_unit_idx in pgroup["units"]:
+                            imp_unit = pgroup["units"][imp_unit_idx]
+                            plane = Plane(id=imp_unit["unitId"], name=self.translation.get_string(imp_unit["name"]))
+                            plane.set_position(MapPosition(imp_unit["x"], imp_unit["y"]))
+                            plane.heading = imp_unit["heading"]
+                            plane.type = imp_unit["type"]
+                            plane.skill = imp_unit["skill"]
+                            plane.livery_id = imp_unit["livery_id"]
+                            plane.x = imp_unit["x"]
+                            plane.y = imp_unit["y"]
+                            plane.alt_type = imp_unit["alt_type"]
+                            plane.alt = imp_unit["alt"]
+                            plane_group.add_unit(unit)
+                        country.add_plane_group(plane_group)
+                col.add_country(country)
             return col
         # blue
         self.coalition["blue"] = imp_coalition(imp_mission["coalition"], "blue")
