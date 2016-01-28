e46bdd5fe52381a47d154bba0b657596edacd02f
Peinthor Rene <peinthor@gmail.com>
2016-01-28 22:18:58 +0100

    fill weather class

--- dcs/mission.py
+++ dcs/mission.py
@@ -57,13 +57,62 @@ class Options:
         return repr(self.options)
 
 
-class Weather:
+class Wind:
+    def __init__(self, direction=0, speed=0):
+        self.direction = direction
+        self.speed = speed
 
     def dict(self):
-        return {}
+        return {"speed": self.speed, "dir": self.direction}
 
-    def __str__(self):
-        return lua.dumps({}, None, 1)
+
+class Weather:
+    def __init__(self):
+        self.atmosphere_type = 0
+        self.wind_at_ground = Wind()
+        self.wind_at_2000 = Wind()
+        self.wind_at_8000 = Wind()
+        self.enable_fog = False
+        self.turbulence_at_ground = 0
+        self.turbulence_at_2000 = 0
+        self.turbulence_at_8000 = 0
+        self.season_temperature = 20
+        self.season_iseason = 1
+        self.type_weather = 0
+        self.qnh = 760
+        self.cyclones = {}
+        self.name = "Summer, clean sky"
+        self.fog_thickness = 0
+        self.fog_visibility = 25
+        self.fog_density = 7
+        self.visibility_distance = 80000
+        self.clouds_thickness = 200
+        self.clouds_density = 0
+        self.clouds_base = 300
+        self.clouds_iprecptns = 0
+
+    def dict(self):
+        d = {}
+        d["atmosphere_type"] = self.atmosphere_type
+        d["wind"] = {"atGround": self.wind_at_ground.dict(),
+                     "at2000": self.wind_at_2000.dict(),
+                     "at8000": self.wind_at_8000.dict()}
+        d["enable_fog"] = self.enable_fog
+        d["turbulence"] = {"atGround": self.turbulence_at_ground,
+                           "at2000": self.turbulence_at_2000,
+                           "at8000": self.turbulence_at_8000}
+        d["season"] = {"iseason": self.season_iseason, "temperature": self.season_temperature}
+        d["type_weather"] = self.type_weather
+        d["qnh"] = self.qnh
+        d["cyclones"] = self.cyclones
+        d["name"] = self.name
+        d["fog"] = {"thickness": self.fog_thickness, "visibility": self.fog_visibility, "density": self.fog_density}
+        d["visibility"] = {"distance": self.visibility_distance}
+        d["clouds"] = {"thickness": self.clouds_thickness,
+                       "density": self.clouds_density,
+                       "base": self.clouds_base,
+                       "iprecptns": self.clouds_iprecptns}
+        return d
 
 
 class Task:
