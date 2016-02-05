e5de46be44a9ec17fa19da1e7a4d1cca867fa37e
Peinthor Rene <peinthor@gmail.com>
2016-02-05 11:49:54 +0100

    cleanup removed strings from translation table

--- dcs/translation.py
+++ dcs/translation.py
@@ -3,6 +3,9 @@ class String:
         self.translation = translation
         self.id = _id
 
+    def __del__(self):
+        self.translation.delete_string(self.id)
+
     def set(self, text, lang='DEFAULT'):
         self.translation.set_string(self.id, text, lang)
         return str(self)
@@ -28,14 +31,15 @@ class Translation:
         self.strings[lang][_id] = string
         return _id
 
-    def get_string(self, _id):
-        return String(_id, self)
-
     def create_string(self, s, lang='DEFAULT'):
         _id = 'DictKey_Translation_{dict_id}'.format(dict_id=self.max_dict_id)
         self.max_dict_id += 1
         self.set_string(_id, s, lang)
         return String(_id, self)
+
+    def delete_string(self, _id):
+        for lang in self.strings:
+            del self.strings[lang][_id]
 
     def languages(self) -> [str]:
         return self.strings.keys()
