5184a606da49b1a83bee22eb7548784815f6b65b
Peinthor Rene <peinthor@gmail.com>
2016-02-28 20:42:48 +0100

    better SA10 site placement

--- dcs/templates.py
+++ dcs/templates.py
@@ -2,6 +2,7 @@ from .countries import *
 from .group import VehicleGroup
 from .vehicle import Vehicle
 from .mission import Mission
+from . import mapping
 
 
 class VehicleTemplate:
@@ -9,13 +10,27 @@ class VehicleTemplate:
         @staticmethod
         def sa10_site(mission: Mission, x, y, heading, prefix=""):
             russia = mission.country("Russia")
-            vg = mission.vehicle_group(russia, prefix + "SA10 site", Russia.Vehicle.S_300PS_5P85C_ln, x, y, heading)
+            vg = mission.vehicle_group(russia, prefix + "SA10 site", Russia.Vehicle.S_300PS_54K6_cp, x, y, heading)
 
-            vg.units[0].name = mission.string("launcher")
-            vg.add_unit(mission.vehicle("radar", Russia.Vehicle.S_300PS_40B6M_tr))
-            vg.add_unit(mission.vehicle("Command post", Russia.Vehicle.S_300PS_54K6_cp))
-            vg.add_unit(mission.vehicle("radar", Russia.Vehicle.S_300PS_64H6E_sr))
+            hdg = 90
+            for i in range(0, 3):  # 3 launchers
+                vx, vy = mapping.point_from_heading(x, y, heading + hdg, 50)
+                u = mission.vehicle("launcher #" + str(i), Russia.Vehicle.S_300PS_5P85C_ln)
+                u.x = vx
+                u.y = vy
+                vg.add_unit(u)
+                hdg += 90
 
-            vg.formation(vg.Formation.Star)
+            vx, vy = mapping.point_from_heading(x, y, heading, 80)
+            u = mission.vehicle("radar", Russia.Vehicle.S_300PS_40B6M_tr)
+            u.x = vx
+            u.y = vy
+            vg.add_unit(u)
+
+            vx, vy = mapping.point_from_heading(x, y, heading + 180, 100)
+            u = mission.vehicle("radar", Russia.Vehicle.S_300PS_64H6E_sr)
+            u.x = vx
+            u.y = vy
+            vg.add_unit(u)
 
             return vg
