9080b5c34bfe27019e4159db7174cad0281bc519
Peinthor Rene <peinthor@gmail.com>
2016-02-29 14:10:10 +0100

    support loadout export from mods

--- tools/weapon_export.lua
+++ tools/weapon_export.lua
@@ -1,21 +1,57 @@
 -- to create the weapons.lua:
--- make sure all files are correctly converted from Cyrilic(Windows 1251) encoding to UTF-8
--- copy wsTypes.lua into weapons.lua
--- attach db_weapons.lua on weapons.lua
--- add a newline to weapons.lua
--- attach db_weapons_data.lua to weapons.lua
--- remove the dofile call in weapons.lua
+-- make sure db_weapons.lua and db_weapons_data.lua are correctly converted from Cyrilic(Windows 1251) encoding to UTF-8
+-- copy db_weapons.lua to weapons.lua
+-- comment the dofile call in weapons.lua
 
 function _(s)
 	return s
 end
 
+ed_path = "C:/Program Files/Eagle Dynamics/DCS World"
+--ed_path = "/home/rp/dcs_data"
+
 db = {}
+dofile(ed_path.."/Scripts/Database/wsTypes.lua")
 dofile('weapons.lua')
+dofile(ed_path.."/Scripts/Database/db_weapons_data.lua")
+
+
+-- mod functions
+function declare_weapon(tbl) end
+function mount_vfs_model_path() end
+function mount_vfs_liveries_path() end
+function mount_vfs_texture_path() end
+function mount_vfs_sound_path() end
+function makeAirplaneCanopyGeometry(a, b, c) end
+function simple_aa_warhead() end
+function pylon() end
+function aircraft_task() end
+function verbose_to_dmg_properties() end
+function add_aircraft() end
+
+function declare_loadout(tbl)
+	if tbl.category ~= nil then
+	   _WEAPON_(tbl.category,tbl)
+	end
+end
+
+-- mod weapons
+dofile(ed_path.."/CoreMods/WWII Units/Weapons.lua")
+
+mod_weapons = {}
+mod_weapons["M2KC_Weapons"] = ed_path.."/CoreMods/aircraft/M-2000C/Weapons"
+mod_weapons["Bf-109K-4"] = ed_path.."/CoreMods/WWII Units"
+mod_weapons["FW-190D9"] = ed_path.."/CoreMods/WWII Units"
+
+
+for k,v in pairs(mod_weapons) do
+    current_mod_path = v
+    dofile(v.."/"..k..".lua")
+end
 
 weapons = {}
 keys = {}
-for j in pairs({1,2,3,4,5,6}) do
+for j in pairs({CAT_BOMBS,CAT_MISSILES,CAT_ROCKETS,CAT_AIR_TO_AIR,CAT_FUEL_TANKS,CAT_PODS}) do
 	for i, v in ipairs(db.Weapons.Categories[j].Launchers) do
 		local pyName = v.displayName
 		if string.sub(v.CLSID, 0, 1) ~= "{" then
@@ -69,7 +105,7 @@ while i <= #keys do
 	local x = keys[i]
 	local s = "    \"" .. weapons[x].clsid .. "\": Weapons." .. x
 	i = i + 1
-	if i < #keys then
+	if i <= #keys then
 		s = s .. ","
 	end
 	print(s)
