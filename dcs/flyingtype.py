fa336cecc57375ffeafc16e9897655ea377cccee
Peinthor Rene <peinthor@gmail.com>
2016-03-01 15:35:20 +0100

    merge unit payloads together

--- dcs/flyingtype.py
+++ dcs/flyingtype.py
@@ -25,10 +25,10 @@ class FlyingType:
     pylons = {}
     payloads = None
     payload_dirs = [
-        os.path.join(os.path.dirname(os.path.realpath(__file__)), "payloads"),
-        os.path.join(os.path.expanduser("~"), "Saved Games\\DCS\\MissionEditor\\UnitPayloads"),
         "C:\\Program Files\\Eagle Dynamics\\DCS World\\MissionEditor\\data\\scripts\\UnitPayloads",
-        "C:\\Program Files\\Eagle Dynamics\\DCS World\\CoreMods\\aircraft\\M-2000C\\UnitPayloads"
+        "C:\\Program Files\\Eagle Dynamics\\DCS World\\CoreMods\\aircraft\\M-2000C\\UnitPayloads",
+        os.path.join(os.path.expanduser("~"), "Saved Games\\DCS\\MissionEditor\\UnitPayloads"),
+        os.path.join(os.path.dirname(os.path.realpath(__file__)), "payloads")
     ]
 
     tasks = ['Nothing']
@@ -42,9 +42,22 @@ class FlyingType:
             payload_filename = os.path.join(payload_dir, cls.id + ".lua")
             if os.path.exists(payload_filename):
                 with open(payload_filename, 'r') as payload:
-                    pays = lua.loads(payload.read())
-                    cls.payloads = pays[list(pays.keys())[0]]
-                    return cls.payloads
+                    payload_main = lua.loads(payload.read())
+                    pays = payload_main[list(payload_main.keys())[0]]
+                    if cls.payloads:
+                        highestkey = max(pays["payloads"].keys()) + 1
+                        for load in pays["payloads"]:
+                            x = [x for x in cls.payloads["payloads"]
+                                 if cls.payloads["payloads"][x]["name"] == pays["payloads"][load]["name"]]
+                            if x:
+                                cls.payloads["payloads"][x[0]] = pays["payloads"][load]
+                            else:
+                                cls.payloads["payloads"][highestkey] = pays["payloads"][load]
+                                highestkey += 1
+                    else:
+                        cls.payloads = pays
+
+        return cls.payloads
 
     @classmethod
     def loadout(cls, _task):
