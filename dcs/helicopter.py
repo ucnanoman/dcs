be555082ad172be18887f7651dd80f389cce9c1d
Peinthor Rene <peinthor@gmail.com>
2016-03-03 22:06:21 +0100

    helicopters have now an ropeLength value

--- dcs/helicopter.py
+++ dcs/helicopter.py
@@ -5,3 +5,13 @@ from .helicopters import HelicopterType, Ka_50
 class Helicopter(FlyingUnit):
     def __init__(self, _id=None, name=None, _type: HelicopterType=Ka_50):
         super(Helicopter, self).__init__(_id, name, _type)
+        self.rope_length = 15
+
+    def load_from_dict(self, d):
+        super(Helicopter, self).load_from_dict(d)
+        self.rope_length = d["ropeLength"]
+
+    def dict(self):
+        d = super(Helicopter, self).dict()
+        d["ropeLength"] = self.rope_length
+        return d
