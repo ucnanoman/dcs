09af5c3096a58c9b29c34d7886dec6e839444b6d
Peinthor Rene <peinthor@gmail.com>
2016-03-03 10:47:52 +0100

    move weather import code into Weather class

--- dcs/weather.py
+++ dcs/weather.py
@@ -59,6 +59,48 @@ class Weather:
         self.clouds_base = 300
         self.clouds_iprecptns = 0
 
+    def load_from_dict(self, dict):
+        self.atmosphere_type = dict["atmosphere_type"]
+        wind = dict.get("wind", {})
+        wind_at_ground = wind.get("atGround", {})
+        wind_at_2000 = wind.get("at2000", {})
+        wind_at_8000 = wind.get("at8000", {})
+        self.wind_at_ground = Wind(wind_at_ground.get("dir", 0), wind_at_ground.get("speed", 0))
+        self.wind_at_2000 = Wind(wind_at_2000.get("dir", 0), wind_at_2000.get("speed", 0))
+        self.wind_at_8000 = Wind(wind_at_8000.get("dir", 0), wind_at_8000.get("speed", 0))
+        self.enable_fog = dict["enable_fog"]
+        turbulence = dict.get("turbulence", {})
+        self.turbulence_at_ground = turbulence.get("atGround", 0)
+        self.turbulence_at_2000 = turbulence.get("at2000", 0)
+        self.turbulence_at_8000 = turbulence.get("at8000", 0)
+        season = dict.get("season", {})
+        self.season_temperature = season.get("temperature", 20)
+        self.season_iseason = season.get("iseason", 1)
+        self.type_weather = dict.get("type_weather", 0)
+        self.qnh = dict.get("qnh", 760)
+        cyclones = dict.get("cyclones", {})
+        for x in cyclones:
+            c = Cyclone()
+            c.centerX = cyclones[x].get("centerX", 0)
+            c.centerZ = cyclones[x].get("centerZ", 0)
+            c.ellipticity = cyclones[x].get("ellipticity", 0)
+            c.pressure_excess = cyclones[x].get("pressure_excess", 0)
+            c.pressure_spread = cyclones[x].get("pressure_spread", 0)
+            c.rotation = cyclones[x].get("rotation", 0)
+            self.cyclones.append(c)
+        self.name = dict.get("name", "Summer, clean sky")
+        fog = dict.get("fog", {})
+        self.fog_thickness = fog.get("thickness", 0)
+        self.fog_visibility = fog.get("visibility", 25)
+        self.fog_density = fog.get("density", 7)
+        visibility = dict.get("visiblity", {})
+        self.visibility_distance = visibility.get("distance", 80000)
+        clouds = dict.get("clouds", {})
+        self.clouds_thickness = clouds.get("thickness", 200)
+        self.clouds_density = clouds.get("density", 0)
+        self.clouds_base = clouds.get("base", 300)
+        self.clouds_iprecptns = clouds.get("iprecptns", 0)
+
     def dict(self):
         d = {}
         d["atmosphere_type"] = self.atmosphere_type
