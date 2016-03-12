c0364d7276c646bcd58f6000c1944621ce356080
Peinthor Rene <peinthor@gmail.com>
2016-03-12 16:28:21 +0100

    small fix on nevada

--- dcs/terrain/nevada.py
+++ dcs/terrain/nevada.py
@@ -3,6 +3,7 @@ from .. import mapping
 
 
 class Nevada(Terrain):
+    center = {"lat": 39.81806, "long": -114.73333}
     bounds = mapping.Rectangle(-497177.656250, -329334.875000, -166934.953125, 209836.890625)
     map_view_default = {
         "center": mapping.Point(-340928.57142857, -55928.571428568),
@@ -13,6 +14,5 @@ class Nevada(Terrain):
         super(Nevada, self).__init__("Nevada")
         # nttr center MGRS
         # 11SPE9400410022
-        self.center = {"lat": 39.81806, "long": -114.73333}
         self.bullseye_blue = {"x": -409931.344, "y": -14024.097}
         self.bullseye_red = {"x": -288293.969, "y": -88022.641}
