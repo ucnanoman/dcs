27687979feb0a7f5fcdffe9773cc662e55b7c95e
Peinthor Rene <peinthor@gmail.com>
2016-03-25 06:37:43 +0100

    add more init packages

--- dcs/__init__.py
+++ dcs/__init__.py
@@ -4,10 +4,13 @@ from . import templates
 from . import countries
 from . import planes
 from . import helicopters
+from . import statics
+from . import vehicles
 from . import terrain
 from . import unit
 from . import unitgroup
 from . import goals
 from . import weather
+from . import point
 from .mapping import Point, Rectangle
 from .mission import Mission
