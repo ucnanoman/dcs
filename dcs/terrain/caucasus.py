77e504ccf7749c7b5bdcab3c9582bef51a3ceb21
Peinthor Rene <peinthor@gmail.com>
2016-04-13 22:19:49 +0200

    specify type

--- dcs/terrain/caucasus.py
+++ dcs/terrain/caucasus.py
@@ -3011,7 +3011,7 @@ class Caucasus(Terrain):
     center = {"lat": 45.12945, "long": 34.26527}
     bounds = mapping.Rectangle(380*1000, -560*1000, -600*1000, 1130*1000)
     map_view_default = MapView(mapping.Point(-255714.28571428, 680571.42857143), 1000000)
-    city_graph = Graph.from_pickle(os.path.join(os.path.dirname(__file__), 'caucasus.p'))
+    city_graph = Graph.from_pickle(os.path.join(os.path.dirname(__file__), 'caucasus.p'))  # type: Graph
 
     def __init__(self):
         super(Caucasus, self).__init__("Caucasus")
