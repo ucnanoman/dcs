4f34d1e049b2a2e2903193fa093ef9a9503aa27c
Peinthor Rene <peinthor@gmail.com>
2017-07-31 20:03:35 +0200

    first export of normandy, version 2.1.1.8244

--- dcs/terrain/__init__.py
+++ dcs/terrain/__init__.py
@@ -1,3 +1,4 @@
 from .terrain import ParkingSlot, Airport, Runway, Terrain, RunwayOccupiedError, NoParkingSlotError, Graph, Node
 from .caucasus import Caucasus
 from .nevada import Nevada
+from .normandy import Normandy
