d130bba9503e38482565d4f0ca47fd8abbcfd553
Peinthor Rene <peinthor@gmail.com>
2019-01-18 22:20:05 +0100

    groundcontrol: add neutrals faction

--- dcs/groundcontrol.py
+++ dcs/groundcontrol.py
@@ -11,6 +11,11 @@ class GroundControl:
         self.blue_jtac = 0
         self.blue_observer = 0
 
+        self.neutrals_game_masters = 0
+        self.neutrals_tactical_commander = 0
+        self.neutrals_jtac = 0
+        self.neutrals_observer = 0
+
     def load_from_dict(self, d):
         if d is None:
             return
@@ -26,13 +31,34 @@ class GroundControl:
         self.blue_jtac = int(d["roles"]["forward_observer"]["blue"])
         self.blue_observer = int(d["roles"]["observer"]["blue"])
 
+        self.neutrals_game_masters = int(d["roles"]["instructor"]["neutrals"])
+        self.neutrals_tactical_commander = int(d["roles"]["artillery_commander"]["neutrals"])
+        self.neutrals_jtac = int(d["roles"]["forward_observer"]["neutrals"])
+        self.neutrals_observer = int(d["roles"]["observer"]["neutrals"])
+
     def dict(self):
         return {
             "isPilotControlVehicles": self.pilot_can_control_vehicles,
             "roles": {
-                "instructor": {"red": self.red_game_masters, "blue": self.blue_game_masters},
-                "artillery_commander": {"red": self.red_tactical_commander, "blue": self.blue_tactical_commander},
-                "forward_observer": {"red": self.red_jtac, "blue": self.blue_jtac},
-                "observer": {"red": self.red_observer, "blue": self.blue_observer},
+                "instructor": {
+                    "red": self.red_game_masters,
+                    "blue": self.blue_game_masters,
+                    "neutrals": self.neutrals_game_masters
+                },
+                "artillery_commander": {
+                    "red": self.red_tactical_commander,
+                    "blue": self.blue_tactical_commander,
+                    "neutrals": self.neutrals_tactical_commander
+                },
+                "forward_observer": {
+                    "red": self.red_jtac,
+                    "blue": self.blue_jtac,
+                    "neutrals": self.neutrals_jtac
+                },
+                "observer": {
+                    "red": self.red_observer,
+                    "blue": self.blue_observer,
+                    "neutrals": self.neutrals_observer
+                },
             }
         }
