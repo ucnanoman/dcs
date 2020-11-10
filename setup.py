fa9195fbccbf96775d108a22c13c3ee2375e4c0b
Rene Peinthor <rene.peinthor@linbit.com>
2020-11-10 17:09:44 +0100

    prepare release 0.12.0

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name="pydcs",
-    version='0.11.0',
+    version='0.12.0',
     description="A Digital Combat Simulator mission builder framework",
     long_description=long_description,
     url='https://github.com/pydcs/dcs',
